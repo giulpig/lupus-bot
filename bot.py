6f9e3682fe3eaeb056ae32d8a5aa2c63d33b254b
giulpig <andrea.codutti20@gmail.com>
2021-02-24 21:28:45 +0100

    spazio

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ def help(update, context):
 
 def echo(update, context):
     """Echo the user message."""
-    update.message.reply_text(update.message.text + "coglione")
+    update.message.reply_text(update.message.text + " coglione")
 
 def error(update, context):
     """Log Errors caused by Updates."""
@@ -44,6 +44,11 @@ def main():
 
     # on noncommand i.e message - echo the message on Telegram
     dp.add_handler(MessageHandler(Filters.text, echo))
+
+
+    #insert code here
+    
+
 
     # log all errors
     dp.add_error_handler(error)
