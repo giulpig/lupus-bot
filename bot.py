441b544c6215f8580f2e23033cd481372df03d17
giulpig <andrea.codutti20@gmail.com>
2021-02-25 12:16:21 +0100

    $comment

--- bot.py
+++ bot.py
@@ -1,6 +1,10 @@
 import logging
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 import os
+from enum import Enum
+
+
+
 PORT = int(os.environ.get('PORT', 5000))
 
 # Enable logging
@@ -10,15 +14,82 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 TOKEN = '1658482800:AAGvwDHhrxIeevOo3a9Ig5uJKFXOGgMDB90'
 
+
+#          0        1         2         3         4
+roles = ["wolf", "bitch", "medium", "madman", "peasant"]
+players = []
+n_players = 0
+
+
+
+
+class State(Enum):
+    STARTED = 0
+    SETPLAYERS = 1
+    JOINED = 2
+    WOLFED = 3
+    BITCHED = 4
+    MEDIUMED = 5
+    MADMANED = 6
+    PEASANTED = 7
+    FINISHED = 8
+    
+
+
 # Define a few command handlers. These usually take the two arguments update and
 # context. Error handlers also receive the raised TelegramError object in error.
 def start(update, context):
     """Send a message when the command /start is issued."""
-    update.message.reply_text('Hi!')
-
+    """Saluto iniziale"""
+    update.message.reply_text('Welcome, FIGA!')
+    
 def help(update, context):
     """Send a message when the command /help is issued."""
-    update.message.reply_text('Help!')
+    update.message.reply_text('Col cazzo che ti aiuto, non ho tempo da perdere, chiedi a Giulio')
+
+
+
+
+
+
+
+
+
+
+def startGame(update, context):
+    update.message.reply_text('How many players?')
+    state = State.STARTED
+    
+    
+def update_from_text(update, context):
+    if state == State.FINISHED:
+        return
+    elif state == State.STARTED:
+        temp = 0
+        try:
+            temp = int(update.message.text)
+        except Exception as e:
+            update.message.reply_text('Wrong input, this must be a number')
+        
+        if(temp < 4 or temp > 30):
+            update.message.reply_text('Wrong input, this must be a positive number between 3 and 30')
+            break
+        
+        else:
+            n_players = temp
+            state = State.SETPLAYERS
+            update.message.reply_text('There are ' + n_players + ' players')
+        return
+        
+        
+        
+
+
+
+
+
+
+
 
 def echo(update, context):
     """Echo the user message."""
@@ -42,13 +113,15 @@ def main():
     # on different commands - answer in Telegram
     dp.add_handler(CommandHandler("start", start))
     dp.add_handler(CommandHandler("help", help))
-
-    # on noncommand i.e message - echo the message on Telegram
-    dp.add_handler(MessageHandler(Filters.text, echo))
-
-
-    #insert code here
     
+    dp.add_handler(CommandHandler("startgame", startGame)) #startGame handler
+    dp.add_handler(CommandHandler("join",      startGame)) #startGame handler
+    dp.add_handler(CommandHandler("stopjoin",  startGame)) #startGame handler
+    dp.add_handler(CommandHandler("getroles",  startGame))
+    
+   
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, update_from_text))    
 
 
     # log all errors
@@ -59,6 +132,12 @@ def main():
                           port=int(PORT),
                           url_path=TOKEN)
     updater.bot.setWebhook('https://protected-inlet-23009.herokuapp.com/' + TOKEN)
+    
+    
+    
+    
+    
+    
 
     # Run the bot until you press Ctrl-C or the process receives SIGINT,
     # SIGTERM or SIGABRT. This should be used most of the time, since
